c635a064f267c9e564c1525fdb374ad079eaa684
Polina Solodkova <polina.solodkova@gmail.com>
2024-09-21 18:31:19 +0300

    feat: add components and style for page

--- lab2/src/Pages/FirstPage/FirstPage.jsx
+++ lab2/src/Pages/FirstPage/FirstPage.jsx
@@ -1,14 +1,24 @@
 // import CatalogCard from "../../Components/CatalogCard/CatalogCard";
 import Header from "../../Components/Header/Header";
 import MainBlock from "../../Components/MainBlock/MainBlock";
- import Footer from "../../Components/Footer/Footer";
-import "./FirstPage.css"
+import Footer from "../../Components/Footer/Footer";
+import "./FirstPage.css";
+import CatalogCard from "../../Components/CatalogCard/CatalogCard";
 const FirstPage = () => {
   return (
     <div className="page">
       <Header first="TARLA" second="WHO IS TARLA" third="HOW IT WORK" />
-      <MainBlock/>
-     <Footer first="INFORMATION" second="LOCATION" third="CATALOG" forth="HOW WORK"/>
+      <MainBlock />
+      <div className="CatalogBlock">
+        <h1>Shop pastry kits</h1>
+        <CatalogCard  />
+      </div>
+      <Footer
+        first="INFORMATION"
+        second="LOCATION"
+        third="CATALOG"
+        forth="HOW WORK"
+      />
     </div>
   );
 };
