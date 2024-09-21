67f424b7650145e120bc2037eb69eab887347dac
Polina Solodkova <polina.solodkova@gmail.com>
2024-09-21 21:08:39 +0300

    feat: add cards on page

--- lab2/src/Pages/SecondPage/SecondPage.jsx
+++ lab2/src/Pages/SecondPage/SecondPage.jsx
@@ -1,11 +1,23 @@
-import './SecondPage.css'
-
+import "./SecondPage.css";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import InformationCard from "../../Components/InformationCard/InformationCard";
 const SecondPage = () => {
-    return ( 
-        <>
-            <div>Second</div>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <div className="page">
+        <Header first="TARLA" second="WHO IS TARLA" third="HOW IT WORK" />
+        <h1>Make an order</h1>
+        <InformationCard />
+        <Footer
+          first="INFORMATION"
+          second="LOCATION"
+          third="CATALOG"
+          forth="HOW WORK"
+        />
+      </div>
+    </>
+  );
+};
+
 export default SecondPage;
