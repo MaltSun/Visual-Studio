93d5041a7c8b19695263f21822a134bda4eee56b
Polina Solodkova <polina.solodkova@gmail.com>
2024-09-21 18:26:48 +0300

    feat: add style for4 catalog card

--- lab2/src/Components/CatalogCard/CatalogCard.jsx
+++ lab2/src/Components/CatalogCard/CatalogCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import Button from "./Pages/ButtonComponent/ButtonComponent";
+import React, { useState, useEffect } from "react";
+import Button from "../Button/Button";
+import "./CatalogCard.css";
 
-export default function CatalogCard(props) {
+export default function CatalogCard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/CatalogCard.json")
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="card disappearSecond">
-        <img src={props.image}></img>
-        <h3>{props.bunName}</h3>
-        <p>{props.bunPrice}</p>
-        <Button information="Information" />
-      </div> 
-  )
+    <div className="cards">
+      {data.map((item, index) => (
+        <div className="disappearSecond" key={index}>
+          <img src={item.photo} alt={item.name}></img>
+          <h3>{item.name}</h3>
+          <p>{item.price}</p>
+          <Button information="Information" />
+        </div>
+      ))}
+    </div>
+  );
 }
