9740c76f0ba60aabc2c75569649384ff488bf606
Polina Solodkova <polina.solodkova@gmail.com>
2024-09-22 18:29:10 +0300

    feat: create error page

---
+++ lab2/src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./ErrorPage.css"
+const ErrorPage = () => {
+  return (
+    <div className="error">
+      <h1> Ooops, something heppened :&#8834;</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
