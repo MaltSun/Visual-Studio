b88bacda493d92c5c673fe83aa124903ebb701f5
Polina Solodkova <polina.solodkova@gmail.com>
2024-09-22 20:31:12 +0300

    feat: implement work with state

--- lab2/src/Components/CatalogCard/CatalogCard.jsx
+++ lab2/src/Components/CatalogCard/CatalogCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Button from "../Button/Button";
 import "./CatalogCard.css";
+import PopupComponent from "../Popup/Popup";
 
 export default function CatalogCard() {
   const [data, setData] = useState([]);
@@ -12,16 +12,60 @@ export default function CatalogCard() {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const addItem = () => {
+    const newItem = {
+      photo: "/Images/almondCroissant.png",
+      name: "NEW ITEM",
+      price: "KIT OF 1 - 10.00",
+      description: "This is a new item",
+    };
+    setData([...data, newItem]);
+  };
+
+  const deleteItem = (index) => {
+    const newData = data.filter((_, i) => i !== index);
+    setData(newData);
+  };
+
+  const editItem = (index, updatedItem) => {
+    const newData = data.map((item, i) => (i === index ? updatedItem : item));
+    setData(newData);
+  };
+
   return (
-    <div className="cards">
-      {data.map((item, index) => (
-        <div className="disappearSecond" key={index}>
-          <img src={item.photo} alt={item.name}></img>
-          <h3>{item.name}</h3>
-          <p>{item.price}</p>
-          <Button information="Information" />
-        </div>
-      ))}
+    <div className="section">
+      <div className="cards">
+        {data.map((item, index) => (
+          <div className="card" key={index}>
+            <img src={item.photo} alt={item.name}></img>
+            <h3>{item.name}</h3>
+            <p>{item.price}</p>
+            <PopupComponent
+              name={item.name}
+              price={item.price}
+              text={item.description}
+            />
+            <button className="button" onClick={() => deleteItem(index)}>
+              Delete
+            </button>
+            <button
+              className="button"
+              onClick={() =>
+                editItem(index, {
+                  ...item,
+                  name: "NEW ITEM",
+                  price: "NEW PRICE",
+                })
+              }
+            >
+              Edit
+            </button>
+          </div>
+        ))}
+      </div>
+      <button className="button" onClick={addItem}>
+        Add New Item
+      </button>
     </div>
   );
 }
